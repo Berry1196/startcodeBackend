8005acff045701eca5db5e195b643a24104a2717
Berry <Sebastianberry11@gmail.com>
2023-04-22 11:29:40 +0200

    made the code more readable in CarResource at method addCar

--- src/main/java/rest/CarResource.java
+++ src/main/java/rest/CarResource.java
@@ -34,18 +34,8 @@ public class CarResource {
     @Consumes("application/json")
     @Produces("application/json")
     public String addCar(String car) {
-        String brand;
-        String model;
-        String numberPlate;
-        try{
-            JsonObject json = JsonParser.parseString(car).getAsJsonObject();
-            brand = json.get("brand").getAsString();
-            model = json.get("model").getAsString();
-            numberPlate = json.get("numberPlate").getAsString();
-        } catch (Exception e) {
-            return "{\"error\":\"Invalid JSON\"}";
-        }
-        CarDTO carDTO = new CarDTO(brand, model, numberPlate);
+
+        CarDTO carDTO = GSON.fromJson(car, CarDTO.class);
         return GSON.toJson(FACADE.create(carDTO));
     }
     @DELETE
